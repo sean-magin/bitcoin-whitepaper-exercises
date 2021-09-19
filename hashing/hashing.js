3b951143b3cf1aa15c87ab85af3bfb6576827262
Sean Magin <sean@arrowburn.co.nz>
2021-09-19 22:06:02 +1200

    low level hashing exercise

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -5,39 +5,67 @@ var crypto = require("crypto");
 // The Power of a Smile
 // by Tupac Shakur
 var poem = [
-	"The power of a gun can kill",
-	"and the power of fire can burn",
-	"the power of wind can chill",
-	"and the power of a mind can learn",
-	"the power of anger can rage",
-	"inside until it tears u apart",
-	"but the power of a smile",
-	"especially yours can heal a frozen heart",
+  "The power of a gun can killz",
+  "and the power of fire can burn",
+  "the power of wind can chill",
+  "and the power of a mind can learn",
+  "the power of anger can rage",
+  "inside until it tears u apart",
+  "but the power of a smile",
+  "especially yours can heal a frozen heart",
 ];
 
 var Blockchain = {
-	blocks: [],
+  blocks: [],
 };
 
 // Genesis block
 Blockchain.blocks.push({
-	index: 0,
-	hash: "000000",
-	data: "",
-	timestamp: Date.now(),
+  index: 0,
+  hash: "000000",
+  data: "",
+  timestamp: Date.now(),
 });
 
+const createBlock = (index, poemLine) => {
+  return Blockchain.blocks.push({
+    index: Blockchain.blocks.length,
+    hash: blockHash(index.toString(), Blockchain.blocks[index].hash, poemLine, Date.now().toString()),
+    prevHash: Blockchain.blocks[Blockchain.blocks.length - 1].hash,
+    data: poemLine,
+    timestamp: Date.now(),
+  });
+};
+
 // TODO: insert each line into blockchain
-// for (let line of poem) {
-// }
-
-// console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
-
-
+for (let [index, line] of poem.entries()) {
+  createBlock(index, line);
+}
 // **********************************
 
-function blockHash(bl) {
-	return crypto.createHash("sha256").update(
-		// TODO: use block data to calculate hash
-	).digest("hex");
+const verifyChain = (Blockchain) => {
+  let passed = true;
+  let lastHash = "000000";
+
+  Blockchain.blocks.forEach((block, index) => {
+    console.log(block);
+    const { prevHash, hash } = block;
+    const genesisBlock = index === 0;
+    const lastBlock = Blockchain.blocks[index - 1];
+    // Checks
+    if (prevHash !== lastHash && !genesisBlock) return (passed = false);
+    if (index === 0 && !genesisBlock) return (passed = false);
+    if (!block.prevHash && !genesisBlock) return (passed = false);
+    if (index < 0) return (passed = false);
+    if (!genesisBlock && prevHash !== lastBlock.hash) return (passed = false);
+    lastHash = hash;
+  });
+  if (passed) return "BLOCKCHAIN VALID! 😄";
+  if (!passed) return "BLOCKCHAIN INVALID! 😔";
+};
+
+console.log(`Result: ${verifyChain(Blockchain)}`);
+
+function blockHash(index, prevHash, data, timestamp) {
+  return crypto.createHash("sha256").update(index, prevHash, data, timestamp).digest("hex");
 }
